da76b6cc1ab6eb2b4e5db8d162add2624a72a179
Thomas Meschke <thomas.meschke@dako.de>
2025-08-04 10:46:03 +0200

    restructure composer file, add scripts

--- dev/cs-fixer/cs-fixer.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-SET BASE_DIR=%~dp0
-
-CMD /C "%BASE_DIR%\..\..\vendor\bin\php-cs-fixer fix %BASE_DIR%\..\..\src --config=%BASE_DIR%\.php-cs-fixer.php"
--- dev/stan/stan.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-SET BASE_DIR=%~dp0
-
-CMD /C "%BASE_DIR%\..\..\vendor\bin\phpstan analyze -c %BASE_DIR%\phpstan.neon.dist"
